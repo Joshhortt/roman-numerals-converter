af09396c495687ce867750e3e8e15481400401d7
Jose Horta <joshhortt@yahoo.com>
2019-05-16 17:39:33 +0100

    indentation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,36 @@
-
-var conversion = function(userNums){
-  var romanNums = ["I","V","X","L","C","D","M"];
-  var arabicNums = [1,5,10,50,100,500,1000];
-  var romanNumsArr = [];
-  for (i = 0; i < arabicNums.length; i++){
-    var unMatch = true;
-    for (j = arabicNums.length; j >= 0; j--){
-      arabicNums.forEach(function(currentNum1){
-        arabicNums.forEach(function(currentNum2){
-          if (userNums === currentNum1 - currentNum2) {
-            unMatch = false;
-          }
-        });
-      });
-        while (unMatch && userNums >= arabicNums[j]) {
-          romanNumsArr.push(romanNums[j]);
-          userNums = userNums - arabicNums[j];
+var conversion = function (userNums) {
+    var romanNums = ["I", "V", "X", "L", "C", "D", "M"];
+    var arabicNums = [1, 5, 10, 50, 100, 500, 1000];
+    var romanNumsArr = [];
+    for (i = 0; i < arabicNums.length; i++) {
+        var unMatch = true;
+        for (j = arabicNums.length; j >= 0; j--) {
+            arabicNums.forEach(function (currentNum1) {
+                arabicNums.forEach(function (currentNum2) {
+                    if (userNums === currentNum1 - currentNum2) {
+                        unMatch = false;
+                    }
+                });
+            });
+            while (unMatch && userNums >= arabicNums[j]) {
+                romanNumsArr.push(romanNums[j]);
+                userNums = userNums - arabicNums[j];
+            }
+            if (userNums === arabicNums[i] - arabicNums[j] && userNums !== 0) {
+                romanNumsArr.push(romanNums[j]);
+                romanNumsArr.push(romanNums[i]);
+            }
         }
-      if (userNums === arabicNums[i] - arabicNums[j] && userNums !== 0) {
-        romanNumsArr.push(romanNums[j]);
-        romanNumsArr.push(romanNums[i]);
-      }
     }
-  }
-  romanNumsArr = romanNumsArr.join("");
-  return romanNumsArr;
+    romanNumsArr = romanNumsArr.join("");
+    return romanNumsArr;
 }
 
 $(document).ready(function () {
-  $('#value-input').submit(function(event){
-    event.preventDefault();
-    var userInput = parseInt($('input').val());
-    var romanNumeric = conversion(userInput);
-    $('#result').text(romanNumeric)
-  });
+    $('#value-input').submit(function (event) {
+        event.preventDefault();
+        var userInput = parseInt($('input').val());
+        var romanNumeric = conversion(userInput);
+        $('#result').text(romanNumeric)
+    });
 });
